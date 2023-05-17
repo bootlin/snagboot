55e4b42f9e88e83dbb3dff92449b3d50faffa536
Thomas Perrot <thomas.perrot@bootlin.com>
2023-05-17 16:18:30 +0200

    install.sh: add virtualenv support
    --user isn't a valid parameter with virtualenv.
    
    In addition, an error is returned on failure.
    
    Signed-off-by: Thomas Perrot <thomas.perrot@bootlin.com>

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
-python3 -m pip install --user build
-python3 -m build
-python3 -m pip install --user dist/snagboot-*-py3-none-any.whl  --force-reinstall
+#!/bin/env sh
+
+if [ -z "${VIRTUAL_ENV_PROMPT}" ]; then
+    python3 -m pip install --user build || exit 1
+else
+    python3 -m pip install build || exit 1
+fi
+
+python3 -m build || exit 2
+python3 -m pip install dist/snagboot-*-py3-none-any.whl  --force-reinstall || exit 3
+
+exit 0
