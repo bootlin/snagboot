c19922ee810e33a71d5db8b212ee6199e99efc34
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2023-05-22 17:29:39 +0200

    src: drop white space in license headers
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- src/snagrecover/firmware/samba_applet.py
+++ src/snagrecover/firmware/samba_applet.py
@@ -1,18 +1,18 @@
 # This file is part of Snagboot
 # Copyright (C) 2023 Bootlin
-# 
+#
 # Written by Romain Gantois <romain.gantois@bootlin.com> in 2023.
-# 
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -21,16 +21,16 @@
 #
 # Copyright (c) 2015-2017, Atmel Corporation All rights reserved.
 # ---------------------------------------------------------------
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # - Redistributions of source code must retain the above copyright notice,
 # this list of conditions and the disclaimer below.
-# 
+#
 # - Atmel's name may not be used to endorse or promote products derived from
 # this software without specific prior written permission.
-# 
+#
 # DISCLAIMER: THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR
 # IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
