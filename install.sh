6082b51f32488b32f625d4945aa148dbb9d818e3
Romain Gantois <romain.gantois@bootlin.com>
2023-05-22 09:55:37 +0200

    Improve install script

--- install.sh
+++ install.sh
@@ -1,14 +1,12 @@
 #!/bin/env sh
 
+EXTRA=""
 if [ -z "${VIRTUAL_ENV_PROMPT}" ]; then
-	python3 -m pip install --user build || exit 1
-	python3 -m build || exit 2
-	python3 -m pip install --user dist/snagboot-*-py3-none-any.whl  --force-reinstall || exit 3
-else
-	python3 -m pip install build || exit 1
-	python3 -m build || exit 2
-	python3 -m pip install dist/snagboot-*-py3-none-any.whl  --force-reinstall || exit 3
+	EXTRA="--user"
 fi
 
+python3 -m pip install $EXTRA build || exit 1
+python3 -m build || exit 2
+python3 -m pip install $EXTRA dist/snagboot-*-py3-none-any.whl  --force-reinstall || exit 3
 
 exit 0
