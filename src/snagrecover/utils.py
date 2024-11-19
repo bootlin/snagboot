bootlin/snagboot

commit 4dd5cd558f4ff47c699903bc7a05bd7940fb1ccd

snagrecover: utils: Fail on USB device address collisions
Sometimes, USB devices can share a single address. For vid:pid addresses, this
is not uncommon, as two devices of the exact same model will most likely share
the same vendor and product IDs. For USB paths, this should not happen but in
some rare cases, libusb can incorrectly assign the same bus number to two
separate root hubs (refer to issue #1521 in the official libusb Github repo for
more details).

If this happens, get_usb() should not just take the first device found and
ignore the other, as this can lead to wildly unexpected behavior (e.g.
snagrecover recovers one device then attempts to flash a different device).

Modify get_usb() and parse_usb_addr() to fail when a USB path collision is
found.

Signed-off-by: Romain Gantois <romain.gantois@bootlin.com>

src/snagrecover/utils.py
@@ -2,6 +2,9 @@ import sys
 import re
 import usb
 import time
+import logging
+logger = logging.getLogger("snagrecover")
+
 import yaml
 import os
 
@@ -73,6 +76,11 @@ def parse_usb_addr(usb_addr: str, find_all=False) -> tuple:
 		if find_all:
 			return usb_paths
 		else:
+			if len(usb_paths) > 1:
+				print(f"Found too many ({len(usb_paths)}) possible results matching {usb_addr}!")
+				logger.error(f"Too many results for address {usb_addr}!\{str(usb_paths)}")
+				access_error("USB", usb_addr)
+
 			return usb_paths[0]
 	else:
 		return parse_usb_path(usb_addr)
@@ -93,7 +101,7 @@ def get_usb(usb_path, error_on_fail=True) -> usb.core.Device:
 
 		nb_devs = len(dev_list)
 
-		if nb_devs > 0:
+		if nb_devs == 1:
 			dev = dev_list[0]
 
 			try:
@@ -101,6 +109,10 @@ def get_usb(usb_path, error_on_fail=True) -> usb.core.Device:
 				return dev
 			except usb.core.USBError:
 				logger.warning(f"Failed to get configuration descriptor for device at {pretty_addr}!")
+
+		elif nb_devs > 1:
+			print(f"Found too many ({nb_devs}) possible results matching {pretty_addr}!")
+			logger.warning(f"Too many results for address {pretty_addr}!\{str(dev_list)}")
 
 		print(f"USB retry {i + 1}/{USB_RETRIES}")
 		time.sleep(USB_INTERVAL)

commit 6122c2bacb54214bf0b21be5374b5fc10f7e3820

snagrecover: utils: Fix various warnings with snagrecover
These probably come from testing on Windows or with outdated Python
versions (?), but otherwise with the latest snagboot sources I get the
following warnings:
$ snagrecover --am335x-setup > /tmp/am335x_usb_setup.sh
~/snagboot/src/snagrecover/utils.py:41: SyntaxWarning: invalid escape sequence '\d'
  path_regex = re.compile('^(\d+)-(\d+)((\.\d+)*)$')
~/snagboot/src/snagrecover/utils.py:81: SyntaxWarning: invalid escape sequence '\{'
  logger.error(f"Too many results for address {usb_addr}!\{str(usb_paths)}")
~/snagboot/src/snagrecover/utils.py:115: SyntaxWarning: invalid escape sequence '\{'
  logger.warning(f"Too many results for address {pretty_addr}!\{str(dev_list)}")

Fixes: 2980889 ("all: Modify CLI and USB helpers to allow passing USB paths as addresses")
Fixes: 4dd5cd5 ("snagrecover: utils: Fail on USB device address collisions")
Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>

src/snagrecover/utils.py
@@ -38,7 +38,7 @@ def parse_usb_ids(usb_id: str) -> tuple:
 	return (vid,pid)
 
 def parse_usb_path(path: str) -> tuple:
-	path_regex = re.compile('^(\d+)-(\d+)((\.\d+)*)$')
+	path_regex = re.compile(r'^(\d+)-(\d+)((\.\d+)*)$')
 	match = path_regex.match(path)
 	if match is None:
 		cli_error(f"failed to parse USB device path {path}")
@@ -78,7 +78,7 @@ def parse_usb_addr(usb_addr: str, find_all=False) -> tuple:
 		else:
 			if len(usb_paths) > 1:
 				print(f"Found too many ({len(usb_paths)}) possible results matching {usb_addr}!")
-				logger.error(f"Too many results for address {usb_addr}!\{str(usb_paths)}")
+				logger.error(f"Too many results for address {usb_addr}!{str(usb_paths)}")
 				access_error("USB", usb_addr)
 
 			return usb_paths[0]
@@ -112,7 +112,7 @@ def get_usb(usb_path, error_on_fail=True) -> usb.core.Device:
 
 		elif nb_devs > 1:
 			print(f"Found too many ({nb_devs}) possible results matching {pretty_addr}!")
-			logger.warning(f"Too many results for address {pretty_addr}!\{str(dev_list)}")
+			logger.warning(f"Too many results for address {pretty_addr}!{str(dev_list)}")
 
 		print(f"USB retry {i + 1}/{USB_RETRIES}")
 		time.sleep(USB_INTERVAL)