bootlin/snagboot

commit 96cd5bb858aae81c62d1f306c1f6c557e30027a3

snagrecover: sunxi: fetch USB device using get_usb()
Currently, the sunxi recovery flow calls usb_ctx.find incorrectly. In any
case, calling get_usb() is more straightforward.

Call get_usb() instead of usb_ctx.find() in the sunxi recovery flow.

Signed-off-by: Romain Gantois <romain.gantois@bootlin.com>

src/snagrecover/utils.py
@@ -10,7 +10,7 @@ from snagrecover.usb import SnagbootUSBContext
 import yaml
 import os
 
-USB_RETRIES = 10
+USB_RETRIES = 9
 USB_INTERVAL = 1
 
 def get_family(soc_model: str) -> str:
@@ -95,11 +95,11 @@ def prettify_usb_addr(usb_addr) -> str:
 	else:
 		return f"{usb_addr[0]:04x}:{usb_addr[1]:04x}"
 
-def get_usb(usb_path, error_on_fail=True) -> usb.core.Device:
+def get_usb(usb_path, error_on_fail=True, retries=USB_RETRIES) -> usb.core.Device:
 	pretty_addr = prettify_usb_addr(usb_path)
 	usb_ctx = SnagbootUSBContext.get_context()
 
-	for i in range(USB_RETRIES):
+	for i in range(retries + 1):
 		usb_ctx = SnagbootUSBContext.rescan()
 		dev_list = list(usb_ctx.find(bus=usb_path[0], port_numbers=usb_path[1]))
 
@@ -120,7 +120,7 @@ def get_usb(usb_path, error_on_fail=True) -> usb.core.Device:
 			logger.info(f"Found too many ({nb_devs}) possible results matching {pretty_addr}!")
 			logger.warning(f"Too many results for address {pretty_addr}!\{str(dev_list)}")
 
-		logger.info(f"USB retry {i + 1}/{USB_RETRIES}")
+		logger.info(f"USB retry {i + 1}/{retries}")
 		time.sleep(USB_INTERVAL)
 
 

src/snagrecover/recoveries/sunxi.py
@@ -25,8 +25,7 @@ logger = logging.getLogger("snagrecover")
 from snagrecover.protocols import fel
 from snagrecover.firmware.firmware import run_firmware
 from snagrecover.config import recovery_config
-from snagrecover.utils import access_error, prettify_usb_addr
-from snagrecover.usb import SnagbootUSBContext
+from snagrecover.utils import get_usb, access_error, prettify_usb_addr
 
 USB_TIMEOUT = 5000
 USB_RETRY = 5
@@ -34,14 +33,11 @@ USB_RETRY = 5
 def main():
 	# Try to reset device
 	usb_addr = recovery_config["usb_path"]
-	bus = usb_addr[0]
-	port_numbers = usb_addr[1]
 
 	# FEL devices seem to require a slightly special retry procedure, which
 	# is why we don't just call get_usb from utils.
 	for i in range(USB_RETRY):
-		usb_ctx = SnagbootUSBContext.rescan()
-		dev = usb_ctx.find(bus=bus, port_numbers=port_numbers)
+		dev = get_usb(usb_addr, error_on_fail=False, retries=0)
 		if dev is None:
 			if i == USB_RETRY - 1:
 				access_error("USB FEL", prettify_usb_addr(usb_addr))
@@ -59,8 +55,7 @@ def main():
 
 	# Try to set device configuration
 	for i in range(USB_RETRY):
-		usb_ctx = SnagbootUSBContext.rescan()
-		dev = usb_ctx.find(bus=bus, port_numbers=port_numbers)
+		dev = get_usb(usb_addr, error_on_fail=False, retries=0)
 		if dev is None:
 			if i == USB_RETRY - 1:
 				access_error("USB FEL", prettify_usb_addr(usb_addr))

commit eec8dfcfeaf327e46ffb2ddd28c161092cf6682e

snagrecover: fix retry count logging
When snagrecover tries to get an usb, it displays a nice log showing the
current number of retries. The total retry count is not displayed
correctly, which results in a wrong display.

Fixes: 96cd5bb ("snagrecover: sunxi: fetch USB device using get_usb()")
Signed-off-by: Louis Chauvet <louis.chauvet@bootlin.com>

src/snagrecover/utils.py
@@ -144,7 +144,7 @@ def get_usb(usb_path, error_on_fail=True, retries=USB_RETRIES, ready_check=activ
 			logger.info(f"Found too many ({nb_devs}) possible results matching {pretty_addr}!")
 			logger.warning(f"Too many results for address {pretty_addr}!\{str(dev_list)}")
 
-		logger.info(f"USB retry {i + 1}/{retries}")
+		logger.info(f"USB retry {i + 1}/{retries + 1}")
 		time.sleep(USB_INTERVAL)
 
 	if error_on_fail: