bootlin/snagboot

commit 78fa71a4154f211176c2890fe2b19e3c846d78ff

snagrecover: utils: Avoid using dev uninitialized when checking permissions
Signed-off-by: Romain Gantois <romain.gantois@bootlin.com>

src/snagrecover/utils.py
@@ -200,7 +200,7 @@ def get_usb(
 
 		time.sleep(USB_INTERVAL)
 
-	if permissions_check(dev):
+	if dev is not None and permissions_check(dev):
 		logger.error(
 			f"USB Device was found at address {pretty_addr} but can't be accessed because of a device file access rights issue."
 		)

commit bb6d7210e39a31c2ee2c6aa3e16bc0d863b99da7

snagrecover: utils: Actually avoid using dev uninitialized
Fixes: 78fa71a snagrecover: utils: Avoid using dev uninitialized when checking permissions
Signed-off-by: Romain Gantois <romain.gantois@bootlin.com>

src/snagrecover/utils.py
@@ -172,6 +172,8 @@ def get_usb(
 	pretty_addr = prettify_usb_addr(usb_path)
 	SnagbootUSBContext.rescan()
 
+	dev = None
+
 	log_access_error = True
 	for i in range(retries + 1):
 		if i > 0: